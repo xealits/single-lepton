a6012e4af1f8cb7c643735b7aa917e47b4476188
Oleksii Toldaiev <alex.toldaiev@cern.ch>
2016-05-24 15:43:10 +0200

    Switched to the old tau-electron isolation discriminator MVA5 (checking the reduction of events)

--- cleanLepton.cc
+++ cleanLepton.cc
@@ -1290,8 +1290,8 @@ for(size_t f=0; f<urls.size();++f)
 			// Consequently, there might be a small bias due to events that are cut by the OldDM and would not be cut by the NewDM
 			if (tau.tauID ("byMediumCombinedIsolationDeltaBetaCorr3Hits")<0.5) continue; // See whether to us the new byMediumPileupWeightedIsolation3Hits that is available only for dynamic strip reconstruction (default in CMSSW_7_4_14)
 			if (tau.tauID ("againstMuonTight3")                          <0.5) continue; // Medium working point not usable. Available values: Loose, Tight
-			// if (tau.tauID ("againstElectronMediumMVA5")                  <0.5) continue; // Tight working point not usable. Avaiable values: VLoose, Loose, Medium
-			if (tau.tauID ("againstElectronMediumMVA6")                  <0.5) continue;
+			if (tau.tauID ("againstElectronMediumMVA5")                  <0.5) continue; // Tight working point not usable. Avaiable values: VLoose, Loose, Medium
+			//if (tau.tauID ("againstElectronMediumMVA6")                  <0.5) continue;
 					
 			// Pixel hits cut (will be available out of the box in new MINIAOD production)
 			{
